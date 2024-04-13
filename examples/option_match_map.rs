5e39e35cb8d21a0918cb9043806505e0448ad9ea
Mathias <email@example.com>
2024-04-13 12:10:58 +0200

    add BEFORE housekeeping => ./examples/option_match_map.rs

---
+++ examples/option_match_map.rs
@@ -0,0 +1,78 @@
+// FROM HERE
+// https://doc.rust-lang.org/rust-by-example/error/option_unwrap/map.html
+
+#![allow(dead_code)]
+
+#[derive(Debug)] enum Food { Apple, Carrot, Potato }
+
+#[derive(Debug)] struct Peeled(Food);
+#[derive(Debug)] struct Chopped(Food);
+#[derive(Debug)] struct Cooked(Food);
+
+// Peeling food. If there isn't any, then return `None`.
+// Otherwise, return the peeled food.
+fn peel(food: Option<Food>) -> Option<Peeled> {
+    match food {
+        Some(food) => Some(Peeled(food)),
+        None       => None,
+    }
+}
+
+// Chopping food. If there isn't any, then return `None`.
+// Otherwise, return the chopped food.
+fn chop(peeled: Option<Peeled>) -> Option<Chopped> {
+    match peeled {
+        Some(Peeled(food)) => Some(Chopped(food)),
+        None               => None,
+    }
+}
+
+// Cooking food. Here, we showcase `map()` instead of `match` for case handling.
+fn cook(chopped: Option<Chopped>) -> Option<Cooked> {
+    chopped.map(|Chopped(food)| Cooked(food))
+}
+
+// A function to peel, chop, and cook food all in sequence.
+// We chain multiple uses of `map()` to simplify the code.
+fn process(food: Option<Food>) -> Option<Cooked> {
+    food.map(|f| Peeled(f))
+        .map(|Peeled(f)| Chopped(f))
+        .map(|Chopped(f)| Cooked(f))
+}
+
+// Check whether there's food or not before trying to eat it!
+fn eat(food: Option<Cooked>) {
+    match food {
+        Some(food) => println!("Mmm. I love {:?}", food),
+        None       => println!("Oh no! It wasn't edible."),
+    }
+}
+
+fn main() {
+    let apple = Some(Food::Apple);
+    let carrot = Some(Food::Carrot);
+    let potato = None;
+
+    let cooked_apple = cook(chop(peel(apple)));
+    let cooked_carrot = cook(chop(peel(carrot)));
+    // Let's try the simpler looking `process()` now.
+    let cooked_potato = process(potato);
+
+    eat(cooked_apple);
+    eat(cooked_carrot);
+    eat(cooked_potato);
+}
+
+/*
+export FILE_NAME="option_match_map.rs"
+export FILE_DIR_NAME="./examples"
+git add $FILE_DIR_NAME/$FILE_NAME
+git commit --all --message="add BEFORE housekeeping => $FILE_DIR_NAME/$FILE_NAME"
+git push
+cargo clippy --fix
+cargo clippy --fix --examples
+cargo fmt -- --emit=files
+git commit --all --message="add AFTER housekeeping => $FILE_DIR_NAME/"
+git push
+cargo run --example $(echo $FILE_NAME | cut -d . -f 1)
+*/
