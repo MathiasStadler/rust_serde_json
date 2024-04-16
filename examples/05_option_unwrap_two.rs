d06ef4fe9bbaf851a4845f6954e32c0bf975ddb2
Mathias <email@example.com>
2024-04-16 15:31:04 +0200

    add AFTER housekeeping => examples/05_option_unwrap_two.rs

--- examples/05_option_unwrap_two.rs
+++ examples/05_option_unwrap_two.rs
@@ -1,15 +1,13 @@
 // FROM HERE
 // https://github.com/saidvandeklundert/LearningRust/blob/master/blog/option-and-result/src/main.rs
 
-
-fn main(){
-let something: Option<&str> = Some("Something");
-        let unwrapped = something.unwrap(); // "Something"
-        println!("{:?}\n{:?}", something, unwrapped);
-        let _nothing: Option<&str> = None;
-        // uncommenting the next line will cause a panic:
-        //nothing.unwrap();
-
+fn main() {
+    let something: Option<&str> = Some("Something");
+    let unwrapped = something.unwrap(); // "Something"
+    println!("{:?}\n{:?}", something, unwrapped);
+    let _nothing: Option<&str> = None;
+    // uncommenting the next line will cause a panic:
+    //nothing.unwrap();
 }
 
 /*
