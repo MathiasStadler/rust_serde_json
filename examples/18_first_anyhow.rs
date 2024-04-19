422bc1bca35b73d0025d25eb2a903818dccd9f8a
Mathias <email@example.com>
2024-04-19 14:50:28 +0200

    -> Add BEFORE housekeeping => examples/18_first_anyhow.rs

--- examples/18_first_anyhow.rs
+++ examples/18_first_anyhow.rs
@@ -1,33 +1,36 @@
+
 use anyhow::{anyhow, Context, Result};
 use serde::{Deserialize, Serialize};
 use std::fs;
 
-fn main() {
-    #[derive(Debug, Deserialize, Serialize)]
-    // FROM HERE
-    // http://saidvandeklundert.net/learn/2021-09-01-rust-option-and-result/
-    // search for => using other crates: anyhow
-    struct Secrets {
-        username: String,
-        password: String,
-    }
 
-    fn get_secrets(s: &str) -> Result<Secrets> {
-        let text = fs::read_to_string(s).context("Secrets file is missing.")?;
-        let secrets: Secrets =
-            serde_json::from_str(&text).context("Problem serializing secrets.")?;
-        if secrets.password.chars().count() < 2 {
-            return Err(anyhow!("Password in secrets file is too short"));
-        }
-        Ok(secrets)
+fn main(){
+#[derive(Debug, Deserialize, Serialize)]
+// FROM HERE
+// http://saidvandeklundert.net/learn/2021-09-01-rust-option-and-result/
+// search for => using other crates: anyhow
+struct Secrets {
+    username: String,
+    password: String,
+}
+
+fn get_secrets(s: &str) -> Result<Secrets> {
+    let text = fs::read_to_string(s).context("Secrets file is missing.")?;
+    let secrets: Secrets =
+        serde_json::from_str(&text).context("Problem serializing secrets.")?;
+    if secrets.password.chars().count() < 2 {
+        return Err(anyhow!("Password in secrets file is too short"));
     }
+    Ok(secrets)
+}
     // Ok
     let _a = get_secrets("/tmp/secrets.json");
     // Result NOT used
     _ = dbg!(_a);
-    // Error
-    let _b = get_secrets("/tmp/missing_file.txt");
+    // Error Secrets file is missing
+    let _b = get_secrets("missing_file.txt");
     _ = dbg!(_b);
+    //Error - Problem serializing secrets
     let c = get_secrets("/tmp/invalid_json.txt");
     _ = dbg!(c);
 }
@@ -44,8 +47,8 @@ cargo clippy --fix
 cargo clippy --fix --examples
 # cargo check --verbose
 # cargo check --verbose --examples
-# cargo check
-# cargo check --examples
+cargo check
+cargo check --examples
 cargo fmt -- --emit=files
 git commit --all --message="-> Add AFTER housekeeping => $FILE_DIR_NAME/$FILE_NAME"
 git push
